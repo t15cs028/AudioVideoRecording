cc47aa9eaf792161501d7f0cf8d24cac70b9891e
t15cs028 <t15cs028@gmail.com>
2018-11-29 19:29:17 +0900

    [add] 構図のデータベース登録を新ファイルに移動 [modify]　ズーミングのバグ修正 [add] gifファイルを再生できるよう機能追加とgifファイル追加

--- app/src/main/java/com/example/dialog/BlockDialogFragment.java
+++ app/src/main/java/com/example/dialog/BlockDialogFragment.java
@@ -1,77 +1,1 @@
-package com.example.dialog;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-
-import com.example.database.DBHelper;
-import com.example.database.Table;
-
-public class BlockDialogFragment extends DialogFragment {
-
-    private DBHelper dbHelper;
-    private Table table;
-
-    public static BlockDialogFragment newInstance(DBHelper dbHelper){
-        BlockDialogFragment fragment = new BlockDialogFragment();
-        fragment.dbHelper = dbHelper;
-        return fragment;
-    }
-
-    public BlockDialogFragment(){
-
-    }
-
-    @Override
-    public void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
-        setRetainInstance(true);
-    }
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final String[] items = {"rename", "delete"};
-        /*
-        int defaultItem = 0; // デフォルトでチェックされているアイテム
-        final List<Integer> checkedItems = new ArrayList<>();
-        checkedItems.add(defaultItem);
-        */
-        return new AlertDialog.Builder(getActivity())
-                .setTitle("Selector")
-                .setItems(items, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // item_which pressed
-                    }
-                })
-                /*
-                .setSingleChoiceItems(items, defaultItem, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        checkedItems.clear();
-                        checkedItems.add(which);
-                    }
-                })
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (!checkedItems.isEmpty()) {
-                            Log.d("checkedItem:", "" + checkedItems.get(0));
-                        }
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                */
-                .show();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        // onPause でダイアログを閉じる場合
-        dismiss();
-    }
-}
+package com.example.dialog;import android.app.AlertDialog;import android.app.Dialog;import android.content.DialogInterface;import android.os.Bundle;import android.support.v4.app.DialogFragment;import com.example.database.DBHelper;import com.example.database.Table;public class BlockDialogFragment extends DialogFragment {    private DBHelper dbHelper;    private Table table;    public static BlockDialogFragment newInstance(DBHelper dbHelper){        BlockDialogFragment fragment = new BlockDialogFragment();        fragment.dbHelper = dbHelper;        return fragment;    }    public BlockDialogFragment(){    }    @Override    public void onCreate(Bundle saveInstanceState){        super.onCreate(saveInstanceState);        setRetainInstance(true);    }    @Override    public Dialog onCreateDialog(Bundle savedInstanceState) {        final String[] items = {"rename", "delete"};        /*        int defaultItem = 0; // デフォルトでチェックされているアイテム        final List<Integer> checkedItems = new ArrayList<>();        checkedItems.add(defaultItem);        */        return new AlertDialog.Builder(getActivity())                .setTitle("Selector")                .setItems(items, new DialogInterface.OnClickListener() {                    @Override                    public void onClick(DialogInterface dialog, int which) {                        // item_which pressed                    }                })                /*                .setSingleChoiceItems(items, defaultItem, new DialogInterface.OnClickListener() {                    @Override                    public void onClick(DialogInterface dialog, int which) {                        checkedItems.clear();                        checkedItems.add(which);                    }                })                .setPositiveButton("OK", new DialogInterface.OnClickListener() {                    @Override                    public void onClick(DialogInterface dialog, int which) {                        if (!checkedItems.isEmpty()) {                            Log.d("checkedItem:", "" + checkedItems.get(0));                        }                    }                })                .setNegativeButton("Cancel", null)                */                .show();    }    @Override    public void onPause() {        super.onPause();        // onPause でダイアログを閉じる場合        dismiss();    }}
--- app/src/main/java/com/example/storyboard/StoryList.java
+++ app/src/main/java/com/example/storyboard/StoryList.java
@@ -1,48 +1,1 @@
-package com.example.storyboard;
-
-import android.graphics.Bitmap;
-
-public class StoryList {
-    private long id;
-    private String name;
-    private Bitmap thumbnail;
-
-    /*
-     * 空のコンストラクタ
-     */
-    public StoryList(){
-
-    }
-
-    public StoryList(long id, String name, Bitmap thumbnail){
-        this.id = id;
-        this.name = name;
-        this.thumbnail = thumbnail;
-    }
-
-
-    public void setId(long id){
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setThumbnail(Bitmap thumbnail){
-        this.thumbnail = thumbnail;
-    }
-
-    public long getId(){
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Bitmap getThumbnail(){
-        return thumbnail;
-    }
-
-}
+package com.example.storyboard;import android.graphics.Bitmap;public class StoryList {    private long id;    private String name;    private Bitmap thumbnail;    /*     * 空のコンストラクタ     */    public StoryList(){    }    public StoryList(long id, String name, Bitmap thumbnail){        this.id = id;        this.name = name;        this.thumbnail = thumbnail;    }    public void setId(long id){        this.id = id;    }    public void setName(String name) {        this.name = name;    }    public void setThumbnail(Bitmap thumbnail){        this.thumbnail = thumbnail;    }    public long getId(){        return id;    }    public String getName() {        return name;    }    public Bitmap getThumbnail(){        return thumbnail;    }}
