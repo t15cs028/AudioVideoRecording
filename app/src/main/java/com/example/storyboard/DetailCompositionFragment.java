9962b3954013674a7a1c95cddbc6bd1f195ac6ff
t15cs028 <t15cs028@gmail.com>
2019-02-08 16:24:13 +0900

    [add] ロングタップでサンプル動画が表示されるように追加 [modify] クリックの冗長的な部分を修正

--- app/src/main/java/com/example/storyboard/DetailCompositionFragment.java
+++ app/src/main/java/com/example/storyboard/DetailCompositionFragment.java
@@ -2,9 +2,6 @@ package com.example.storyboard;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.text.SpannableStringBuilder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,20 +14,18 @@ import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.example.camera.R;
 import com.example.database.Composition;
 import com.example.database.DBHelper;
-import com.example.database.Story;
+import com.example.database.Sample;
 import com.example.database.Table;
 
-import static com.example.database.DBHelper.URL;
-
-public class DetailComposition extends Fragment {
+public class DetailCompositionFragment extends Fragment {
 
     private DBHelper dbHelper;
     private int storyBoardNumber;
     private int compositionID;
     private View rootView;
 
-    public static DetailComposition newInstance(DBHelper dbHelper, int storyBoardNumber, int compositionID){
-        DetailComposition fragment = new DetailComposition();
+    public static DetailCompositionFragment newInstance(DBHelper dbHelper, int storyBoardNumber, int compositionID){
+        DetailCompositionFragment fragment = new DetailCompositionFragment();
         fragment.dbHelper = dbHelper;
         fragment.storyBoardNumber = storyBoardNumber;
         fragment.compositionID = compositionID;
@@ -57,13 +52,29 @@ public class DetailComposition extends Fragment {
                 Table.COMPOSITION, Composition.THUMB_ID.getName(),
                 Composition.ID.getName(), String.valueOf(compositionID)
         )[0];
+        System.out.println(String.valueOf(compositionID));
         GlideDrawableImageViewTarget target = new GlideDrawableImageViewTarget(image);
         Glide.with(getContext()).load(Integer.parseInt(layout_id)).into(target);
+
+        String sample_movie[] = dbHelper.getColumn(
+                Table.SAMPLE, Sample.FILE_ID.getName(),
+                Sample.COMPOSITION_ID.getName(), String.valueOf(compositionID)
+        );
+
+        image = (ImageView) rootView.findViewById(R.id.sample1);
+        target = new GlideDrawableImageViewTarget(image);
+        Glide.with(getContext()).load(Integer.parseInt(sample_movie[0])).into(target);
+        /*
+        image = (ImageView) rootView.findViewById(R.id.sample2);
+        target = new GlideDrawableImageViewTarget(image);
+        Glide.with(getContext()).load(Integer.parseInt(sample_movie[1])).into(target);
+        */
 
 
         Button button = (Button) view.findViewById(R.id.enter);
         final EditText name = (EditText) view.findViewById(R.id.name);
         final EditText desc = (EditText) view.findViewById(R.id.description);
+
 
 
         button.setOnClickListener(onClickListener);
